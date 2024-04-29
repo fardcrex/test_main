29391a7aac8a217e30bcd8ab04c725fa5340e112
Jair Conislla <jacobo_9410@hotmail.com>
2024-04-29 12:51:06 -0500

    Update clean_code.dart

--- clean_code.dart
+++ clean_code.dart
@@ -1,28 +1,56 @@
 void main() {
-  sendWelcomeText(MediaType.email,'Alonso');
-  
+  sendWelcomeText(MediaType.email, 'Alonso');
 }
 
 void sendWelcomeText(MediaType media, String name) {
-  if (media == MediaType.facebook) {
-    print('Check facebook');
-    print('Execute code auth ...');
-    print('Welcome to facebook $name');
-  }
-  if (media == MediaType.email) {
-    print('Check EmailAddress');
-    print('Execute code validation ...');
-    print('Welcome to facebook $name');
-  }
-  /*   if (media == MediaType.whatssap) {
-    print('Check number');
-    print('Execute code sms ...');
-    print('Welcome to Whatsapp $name');
-  } */
+  final messageSender = MediaExecution.fromType(media);
+
+  messageSender.execution(name);
 }
 
 enum MediaType {
   facebook,
   email,
- // whatssap,
+  whatssap,
+}
+
+abstract class MediaExecution {
+  const MediaExecution();
+
+  void execution(String name);
+
+  factory MediaExecution.fromType(MediaType media) {
+    return switch (media) {
+      MediaType.facebook => MediaFacebook(),
+      MediaType.email => MediaEmail(),
+      MediaType.whatssap => MediaWhatsapp(),
+    };
+  }
+}
+
+class MediaFacebook extends MediaExecution {
+  @override
+  void execution(String name) {
+    print('Check facebook');
+    print('Execute code auth ...');
+    print('Welcome to facebook $name');
+  }
+}
+
+class MediaEmail extends MediaExecution {
+  @override
+  void execution(String name) {
+    print('Check EmailAddress');
+    print('Execute code validation ...');
+    print('Welcome to facebook $name');
+  }
+}
+
+class MediaWhatsapp extends MediaExecution {
+  @override
+  void execution(String name) {
+    print('Check number');
+    print('Execute code sms ...');
+    print('Welcome to Whatsapp $name');
+  }
 }
