2f4f06c68cab6632a8b86427d6158f157e0e40a0
Jair Conislla <jacobo_9410@hotmail.com>
2024-04-29 12:10:18 -0500

    Create clean_code.dart

---
+++ clean_code.dart
@@ -0,0 +1,28 @@
+void main() {
+  sendWelcomeText(MediaType.email,'Alonso');
+  
+}
+
+void sendWelcomeText(MediaType media, String name) {
+  if (media == MediaType.facebook) {
+    print('Check facebook');
+    print('Execute code auth ...');
+    print('Welcome to facebook $name');
+  }
+  if (media == MediaType.email) {
+    print('Check EmailAddress');
+    print('Execute code validation ...');
+    print('Welcome to facebook $name');
+  }
+  /*   if (media == MediaType.whatssap) {
+    print('Check number');
+    print('Execute code sms ...');
+    print('Welcome to Whatsapp $name');
+  } */
+}
+
+enum MediaType {
+  facebook,
+  email,
+  whatssap,
+}
